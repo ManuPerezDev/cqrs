49e8be5650ae0b2a31606212a55880baf0d6b001
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-02-12 18:07:27 +0000

    change handler responsibility

--- src/commandBusExample/apps/book/handlers/CreateBookCommandHandler.ts
+++ src/commandBusExample/apps/book/handlers/CreateBookCommandHandler.ts
@@ -1,6 +1,5 @@
 import { CommandHandler } from './CommandHandler'
 import { CreateBookCommand } from '../commands/CreateBookCommand'
-import { Book } from '../../../book/domain/Book'
 import { Name } from '../../../book/domain/valueObjects/Name'
 import { Author } from '../../../book/domain/valueObjects/Author'
 import { CreateBook } from '../../../book/application/CreateBook'
@@ -15,8 +14,6 @@ export class CreateBookCommandHandler implements CommandHandler {
   handle (saveBookCommand: CreateBookCommand): void {
     const name = new Name(saveBookCommand.name)
     const author = new Author(saveBookCommand.author)
-    const book = new Book(name, author)
-
-    this.service.run(book)
+    this.service.run(name, author)
   }
 }
--- src/commandBusExample/book/application/CreateBook.ts
+++ src/commandBusExample/book/application/CreateBook.ts
@@ -1,5 +1,7 @@
 import { BookRepository } from '../domain/BookRepository'
 import { Book } from '../domain/Book'
+import { Name } from '../domain/valueObjects/Name'
+import { Author } from '../domain/valueObjects/Author'
 
 export class CreateBook {
   private repository: BookRepository
@@ -8,8 +10,9 @@ export class CreateBook {
     this.repository = repository
   }
 
-  run (book: Book) {
+  run (name: Name, author: Author) {
     console.log('6.- Execute use case')
+    const book = new Book(name, author)
     this.repository.save(book)
   }
 }
